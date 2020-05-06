87cf031ef9ad669691f4eb2ce59b682852e9399d
diegoorbe96 <dieorbe96sw@gmail.com>
2020-05-06 18:00:44 -0300

    Update 8
    -Agregado Light mode.
    -Creado logica.js para todo lo que refiere a la logica dle juego.
    
    TODO:
    -Completar js para la grafica

--- js/app.js
+++ js/app.js
@@ -1,21 +1,161 @@
-var matriz_juego
-var cantidad_targets_max
-var cantidad_targets_actual
-var cantidad_seleccionados
 var timer_cronometro
+var partida_en_progreso = false
 var timer_generacion
 var colores = ["#2038b2", "#ce1616", "#4ed01a", "#c53ace"]
+var estilo_actual = "light"
+var logica
 
-//Variables resultados.
-var cantidad_errores
-var deespawn_time
-var promedio_diferencias
-var puntaje_final
-var tiempo_transcurrido
+load_ls()
+
+
+function updateCant() {
+    localStorage.setItem("record-cant", document.getElementById("targets").value)
+}
+
+function updateDif() {
+    localStorage.setItem("record-dif", document.getElementById("dificultad_juego").value)
+}
+
+function load_ls() {
+    //Recupero el estilo
+    var estilo_rec = localStorage.getItem('record-estilo');
+    if (estilo_rec != null) {
+        if (estilo_rec != estilo_actual) {
+            boton_estilo()
+        }
+    }
+
+    //Recupero la dificultad
+    var dif = localStorage.getItem("record-dif");
+    if (dif != null) {
+        document.getElementById("dificultad_juego").value = dif
+    }
+
+    //Recupero la cantidad de objetivos:
+    var cant = localStorage.getItem("record-cant")
+    if (cant != null) {
+        document.getElementById("targets").value = cant
+    }
+}
+
+
+function actualizar_elementos() {
+    //Elijo un elemento random
+    var i_rand = Math.floor(Math.random() * (logica.matriz_juego.length))
+    var j_rand = Math.floor(Math.random() * (logica.matriz_juego.length))
+
+    if (logica.matriz_juego[i_rand][j_rand] == 0) {
+        //Si la celda esta libre, le pongo un cuadradito
+        logica.actualizar_matriz(i_rand, j_rand);
+        var color_rand = Math.floor(Math.random() * (colores.length))
+        document.getElementById("c" + (i_rand + 1) + "-" + (j_rand + 1)).style.backgroundColor = colores[color_rand];
+    }
+
+
+    if (logica.cantidad_targets_actual >= logica.cantidad_targets_max) {
+        clearInterval(timer_generacion)
+    }
+
+}
+
+
+
+
+
+
+
+function fin_de_juego() {
+    //Aca finalizo el juego.
+    clearInterval(timer_cronometro)
+    document.getElementById("resultados").style.display = "block"
+    document.getElementById("cronometro").style.display = "none"
+    logica.tiempo_transcurrido = document.getElementById("cronometro").textContent
+    logica.promedio_diferencias = logica.promedio_apuntado()
+        //Actualizo el modal con los puntajes.
+    document.getElementById("tiempo_a_mostrar").textContent = logica.tiempo_transcurrido + " seg."
+    document.getElementById("errores_a_mostrar").textContent = logica.cantidad_errores
+    document.getElementById("promedio_a_mostrar").textContent = logica.promedio_diferencias + " seg."
+    var arr = logica.calculoPuntaje()
+    document.getElementById("puntaje_a_mostrar").textContent = arr[0]
+    document.getElementById("item-modal-flavour").textContent = arr[1]
+}
+
+
+
+
+
+
+
+
+
+
+//----------------------------------------Interaccion de Usuario--------------------------------------------
+
+function comienzoJuego() {
+
+    partida_en_progreso = true
+        //Creo la grid
+    var lim
+    var dificultad = document.getElementById("dificultad_juego").value
+    this.logica = new Logica(parseInt(document.getElementById("targets").value), dificultad)
+    switch (dificultad) {
+        case "1":
+            lim = 6;
+            break
+        case "2":
+            lim = 11;
+            break;
+        case "3":
+            lim = 16;
+            break;
+    }
+
+
+    crear_grid(lim)
+
+
+    //Hago desaparecer las partes de input y hago aparecer el cronometro:
+    document.getElementById("container_inputs").style.display = "none"
+    document.getElementById("cronometro").style.display = "block"
+        // document.getElementById("cronometro").style.visibility = "visible"
+        //Inicializo el cronometro.
+    document.getElementById("cronometro").textContent = "0.00"
+    timer_cronometro = setInterval(actualizar_timer, 10)
+        //Inicio la generacion de elementos segun dificultad elegida.
+    switch (dificultad) {
+        case "1":
+            timer_generacion = setInterval(actualizar_elementos, 500)
+            break;
+        case "2":
+            timer_generacion = setInterval(actualizar_elementos, 450)
+            break;
+        case "3":
+            timer_generacion = setInterval(actualizar_elementos, 400)
+            break;
+    }
+
+
+
+}
+
+
+function mostrarResultados() {
+    $('#modal_resultados').modal('show')
+        //Muestro los elementos de a uno.
+
+    setTimeout(() => { fadeIn(document.getElementById("item-modal-tiempo"), 3000); }, 1000);
+    setTimeout(() => { fadeIn(document.getElementById("item-modal-errores"), 3000); }, 1000);
+    setTimeout(() => { fadeIn(document.getElementById("item-modal-promedio"), 3000); }, 1000);
+    setTimeout(() => {
+        fadeIn(document.getElementById("item-modal-puntaje"), 3000);
+        fadeIn(document.getElementById("item-modal-flavour"), 3000);
+    }, 2000);
+
+}
 
 function pressedButton() {
 
-    if (cantidad_seleccionados == cantidad_targets_max) {
+    if (logica.cantidad_seleccionados == logica.cantidad_targets_max) {
         return false
     }
 
@@ -28,22 +168,83 @@ function pressedButton() {
     var id_recortado = cell_id.substring(1, cell_id.length).split("-")
     var i = parseInt(id_recortado[0]) - 1
     var j = parseInt(id_recortado[1]) - 1
-    if (matriz_juego[i][j] == 1) {
-        boton.style.backgroundColor = '#171716'
-        matriz_juego[i][j] = 0
-        deespawn_time[cantidad_seleccionados] = document.getElementById("cronometro").textContent
+    if (logica.matriz_juego[i][j] == 1) {
+        if (estilo_actual == "dark") {
+            boton.style.backgroundColor = '#171716'
+        } else {
+            boton.style.backgroundColor = "#c5cad1"
+        }
 
-        cantidad_seleccionados++
+        logica.agregar_deespawn(document.getElementById("cronometro").textContent)
+        logica.actualizar_matriz(i, j)
     } else {
-        cantidad_errores++;
+        logica.update_errores()
     }
 
 
-    if (cantidad_seleccionados == cantidad_targets_max) {
+    if (logica.cantidad_seleccionados == logica.cantidad_targets_max) {
         fin_de_juego();
     }
 
 }
+
+
+
+
+function boton_estilo() {
+    if (!partida_en_progreso) {
+        if (estilo_actual == "dark") {
+            estilo_actual = "light"
+            document.getElementById("imagen-estilo").src = "dark.png"
+            cambiar_style("css/light.css")
+            localStorage.setItem("record-estilo", "light")
+        } else {
+            estilo_actual = "dark"
+            document.getElementById("imagen-estilo").src = "light.png"
+            cambiar_style("css/dark.css")
+            localStorage.setItem("record-estilo", "dark")
+        }
+    }
+}
+
+
+
+function reinicio_juego() {
+    //Vuelvo a habilitar todo lo de la pantalla principal
+    document.getElementById("container_inputs").style.display = "block"
+    partida_en_progreso = false
+
+    //Saco el Modal y el boton de mostrar resultados.
+    $('#modal_resultados').modal("hide")
+    document.getElementById("resultados").style.display = "none"
+    document.getElementById("item-modal-tiempo").style.opacity = 0;
+    document.getElementById("item-modal-errores").style.opacity = 0;
+    document.getElementById("item-modal-promedio").style.opacity = 0;
+    document.getElementById("item-modal-puntaje").style.opacity = 0;
+    document.getElementById("item-modal-flavour").style.opacity = 0;
+
+
+    //Vuelvo a habilitar los valores por defecto
+    document.getElementById("tabla_juego").remove()
+    logica = null
+
+}
+//----------------------------------------------------------------------------------------------------------
+
+//----------------------------------------Funciones Auxiliares Graficas-------------------------------------
+//CAMBIAR: Mantiene el color del fondo para el input "Cantidad de Objetivos"
+function mantenerFondo() {
+    if (estilo_actual == "dark") {
+        document.getElementById("targets").style.backgroundColor = "#333030"
+        document.getElementById("targets").style.color = "white"
+    } else {
+        document.getElementById("targets").style.backgroundColor = "white"
+        document.getElementById("targets").style.color = "black"
+
+    }
+
+}
+
 
 
 function crear_grid(lim) {
@@ -73,53 +274,10 @@ function crear_grid(lim) {
 
 }
 
-function comienzoJuego() {
-    cantidad_targets_max = parseInt(document.getElementById("targets").value)
-    cantidad_targets_actual = 0
-    cantidad_seleccionados = 0
-    cantidad_errores = 0
-        //Creo la grid
-    var lim
-    var dificultad = document.getElementById("dificultad_juego").value
-    if (dificultad == "1") {
-        lim = 6
-    } else if (dificultad == "2") {
-        lim = 11
-    } else if (dificultad == "3") {
-        lim = 16
-    }
-    crear_grid(lim)
-
-    //Creo la matriz de la logica del juego
-    matriz_juego = [];
-    for (var i = 0; i < lim - 1; i++) {
-        matriz_juego[i] = new Array(lim - 1);
-        for (var j = 0; j < lim - 1; j++) {
-            matriz_juego[i][j] = 0;
-        }
-    }
-    //Creo el listado de clickeos
-    deespawn_time = []
-
-    //Hago desaparecer las partes de input y hago aparecer el cronometro:
-    document.getElementById("container_inputs").style.display = "none"
-    document.getElementById("cronometro").style.display = "block"
-        // document.getElementById("cronometro").style.visibility = "visible"
-        //Inicializo el cronometro.
-    document.getElementById("cronometro").textContent = "0.00"
-    timer_cronometro = setInterval(actualizar_timer, 10)
-        //Inicio la generacion de elementos segun dificultad elegida.
-    if (dificultad == "1") {
-        timer_generacion = setInterval(actualizar_elementos, 500)
-    } else if (dificultad == "2") {
-        timer_generacion = setInterval(actualizar_elementos, 450)
-    } else if (dificultad == "3") {
-        timer_generacion = setInterval(actualizar_elementos, 400)
-    }
-
-
-
+function cambiar_style(css_link) {
+    document.getElementById("stylesheet").href = css_link
 }
+
 
 function actualizar_timer() {
     var cronometro = document.getElementById("cronometro");
@@ -128,43 +286,7 @@ function actualizar_timer() {
 }
 
 
-function actualizar_elementos() {
-    //Elijo un elemento random
-    var i_rand = Math.floor(Math.random() * (matriz_juego.length))
-    var j_rand = Math.floor(Math.random() * (matriz_juego.length))
-
-    if (matriz_juego[i_rand][j_rand] == 0) {
-        //Si la celda esta libre, le pongo un cuadradito
-        matriz_juego[i_rand][j_rand] = 1;
-        var color_rand = Math.floor(Math.random() * (colores.length))
-        document.getElementById("c" + (i_rand + 1) + "-" + (j_rand + 1)).style.backgroundColor = colores[color_rand];
-        cantidad_targets_actual++;
-    }
-
-
-    if (cantidad_targets_actual >= cantidad_targets_max) {
-        clearInterval(timer_generacion)
-    }
-
-}
-
-function mostrarResultados() {
-    $('#modal_resultados').modal('show')
-        //Muestro los elementos de a uno.
-
-    setTimeout(() => { fadeIn(document.getElementById("item-modal-tiempo"), 3000); }, 1000);
-    setTimeout(() => { fadeIn(document.getElementById("item-modal-errores"), 3000); }, 1000);
-    setTimeout(() => { fadeIn(document.getElementById("item-modal-promedio"), 3000); }, 1000);
-    setTimeout(() => {
-        fadeIn(document.getElementById("item-modal-puntaje"), 3000);
-        fadeIn(document.getElementById("item-modal-flavour"), 3000);
-    }, 2000);
-
-}
-
-
 function fadeIn(el, i) {
-
     var go = function(i) {
         setTimeout(function() {
             el.style.opacity = i;
@@ -172,159 +294,15 @@ function fadeIn(el, i) {
     };
     for (i = 0; i <= 1; i = i + 0.01) go(i);
 }
-
-function reinicio_juego() {
-    //Vuelvo a habilitar todo lo de la pantalla principal
-    document.getElementById("container_inputs").style.display = "block"
-        //Saco el Modal y el boton de mostrar resultados.
-    $('#modal_resultados').modal("hide")
-    document.getElementById("resultados").style.display = "none"
-    document.getElementById("item-modal-tiempo").style.opacity = 0;
-    document.getElementById("item-modal-errores").style.opacity = 0;
-    document.getElementById("item-modal-promedio").style.opacity = 0;
-    document.getElementById("item-modal-puntaje").style.opacity = 0;
-    document.getElementById("item-modal-flavour").style.opacity = 0;
+//-----------------------------------------------------------------------------------------------------------
 
 
-    //Vuelvo a habilitar los valores por defecto
-    document.getElementById("tabla_juego").remove()
-
-
-}
-
-function fin_de_juego() {
-    //Aca finalizo el juego.
-    clearInterval(timer_cronometro)
-    document.getElementById("resultados").style.display = "block"
-    document.getElementById("cronometro").style.display = "none"
-    tiempo_transcurrido = document.getElementById("cronometro").textContent
-    promedio_diferencias = promedio_apuntado()
-
-    //Actualizo el modal con los puntajes.
-    document.getElementById("tiempo_a_mostrar").textContent = tiempo_transcurrido + " seg."
-    document.getElementById("errores_a_mostrar").textContent = cantidad_errores
-    document.getElementById("promedio_a_mostrar").textContent = promedio_diferencias + " seg."
-    var arr = calculoPuntaje()
-    document.getElementById("puntaje_a_mostrar").textContent = arr[0]
-    document.getElementById("item-modal-flavour").textContent = arr[1]
-}
+//----------------------------------------Funciones Auxiliares Logica----------------------------------------
 
 
 
-//La funcion calcula el puntaje final y el flavor text que felicita o reta al usuario.
-function calculoPuntaje() {
-    var salida = []
-        //Hago el calculo del puntaje a mapear
-    var parte_t = ((1 / 3) * (tiempo_transcurrido / cantidad_targets_max))
-    var parte_e = ((1 / 3) * (cantidad_errores / cantidad_targets_max))
-    var parte_p = ((1 / 3) * (promedio_diferencias))
-    puntaje_final = 1 / (parte_p + parte_e + parte_t)
-
-    //Hago el chequeo de dificultad y asigno el puntaje final
-    var dificultad = document.getElementById("dificultad_juego").value
-    if (dificultad == "1") {
-        //Calculo de dificultad facil
-        if (puntaje_final <= 2) {
-            salida[0] = "F"
-        } else if (puntaje_final > 2 && puntaje_final <= 2.422) {
-            salida[0] = "E"
-        } else if (puntaje_final > 2.422 && puntaje_final <= 2.535) {
-            salida[0] = "D"
-        } else if (puntaje_final > 2.535 && puntaje_final <= 2.637) {
-            salida[0] = "C"
-        } else if (puntaje_final > 2.637 && puntaje_final <= 2.836) {
-            salida[0] = "B"
-        } else if (puntaje_final > 2.836 && puntaje_final <= 2.889) {
-            salida[0] = "A"
-        } else {
-            salida[0] = "S"
-        }
-    } else if (dificultad == "2") {
-        //Calculo de dificultad media.
-        if (puntaje_final <= 2.339) {
-            salida[0] = "F"
-        } else if (puntaje_final > 2.339 && puntaje_final <= 2.533) {
-            salida[0] = "E"
-        } else if (puntaje_final > 2.533 && puntaje_final <= 2.709) {
-            salida[0] = "D"
-        } else if (puntaje_final > 2.709 && puntaje_final <= 2.817) {
-            salida[0] = "C"
-        } else if (puntaje_final > 2.817 && puntaje_final <= 2.959) {
-            salida[0] = "B"
-        } else if (puntaje_final > 2.959 && puntaje_final <= 3.101) {
-            salida[0] = "A"
-        } else {
-            salida[0] = "S"
-        }
-    } else if (dificultad == "3") {
-        //Calculo de dificultad dificil
-        if (puntaje_final <= 2.298) {
-            salida[0] = "F"
-        } else if (puntaje_final > 2.298 && puntaje_final <= 2.353) {
-            salida[0] = "E"
-        } else if (puntaje_final > 2.353 && puntaje_final <= 2.512) {
-            salida[0] = "D"
-        } else if (puntaje_final > 2.512 && puntaje_final <= 2.670) {
-            salida[0] = "C"
-        } else if (puntaje_final > 2.670 && puntaje_final <= 2.762) {
-            salida[0] = "B"
-        } else if (puntaje_final > 2.762 && puntaje_final <= 3.219) {
-            salida[0] = "A"
-        } else {
-            salida[0] = "S"
-        }
-    }
-    console.log(salida[0])
 
 
-    //Defino el flavour text en base al puntaje que consiguio
-    switch (salida[0]) {
-        case "F":
-            salida[1] = "Recomendamos que trabajes en tus reflejos, intente nuevamente."
-            break
-        case "E":
-            salida[1] = "Todavia se puede mejorar, intente nuevamente."
-            break
-        case "D":
-            salida[1] = "Todavia se puede mejorar, intente nuevamente."
-            break
-        case "C":
-            salida[1] = "Todavia se puede mejorar, intente nuevamente."
-            break
-        case "B":
-            salida[1] = "Bien! Tiene reflejos por encima de la media, intente nuevamente."
-            break
-        case "A":
-            salida[1] = "Excelente! Intente nuevamente con una mayor dificultad o mas objetivos."
-            break
-        case "S":
-            salida[1] = "Sus reflejos y punteria son excelentes, felicitaciones!"
-            break
-    }
-
-    return salida
-}
-
-//La funcion calcula el promedio de las diferencias en el apuntado de un objetivo a otro
-function promedio_apuntado() {
-    //Genero un arreglo de diferencias
-    var diff = []
-    for (i = 0; i < deespawn_time.length - 1; i++) {
-        var elem_i = parseFloat(deespawn_time[i])
-        var elem_i1 = parseFloat(deespawn_time[i + 1])
-        diff.push(parseFloat((elem_i1 - elem_i).toFixed(2)))
-    }
-    var suma = 0
-    for (i = 0; i < diff.length; i++) {
-        suma += diff[i]
-    }
-    return (suma / diff.length).toFixed(2)
-}
 
 
-//CAMBIAR A LA MIERDA.
-function mantenerFondo() {
-    document.getElementById("targets").style.backgroundColor = "#333030"
-    document.getElementById("targets").style.color = "white"
-
-}
+//-----------------------------------------------------------------------------------------------------------
---
+++ js/logica.js
@@ -0,0 +1,164 @@
+class Logica {
+    matriz_juego;
+    cantidad_targets_max;
+    cantidad_targets_actual;
+    cantidad_seleccionados;
+    cantidad_errores;
+    deespawn_time;
+    promedio_diferencias;
+    puntaje_final;
+    tiempo_transcurrido;
+    dificultad;
+
+    constructor(cantidad_targets_max, dificultad) {
+        this.cantidad_targets_max = cantidad_targets_max;
+        this.cantidad_targets_actual = 0;
+        this.cantidad_seleccionados = 0;
+        this.cantidad_errores = 0;
+        this.dificultad = dificultad;
+        this.crear_matriz_juego();
+        this.deespawn_time = [];
+    }
+
+    crear_matriz_juego() {
+        var lim
+        switch (this.dificultad) {
+            case "1":
+                lim = 6;
+                break
+            case "2":
+                lim = 11;
+                break;
+            case "3":
+                lim = 16;
+                break;
+        }
+        //Creo la matriz de la logica del juego
+        this.matriz_juego = [];
+        for (var i = 0; i < lim - 1; i++) {
+            this.matriz_juego[i] = new Array(lim - 1);
+            for (var j = 0; j < lim - 1; j++) {
+                this.matriz_juego[i][j] = 0;
+            }
+        }
+
+    }
+
+
+    actualizar_matriz(i, j) {
+        if (this.matriz_juego[i][j] == 0) {
+            //Espacio libre, me llamaron para generar nuevo cuadrado
+            this.matriz_juego[i][j] = 1;
+            this.cantidad_targets_actual++;
+        } else {
+            //Espacio ocupado, me llamaron para eliminar cuadrado
+            this.matriz_juego[i][j] = 0;
+            this.cantidad_seleccionados++;
+        }
+    }
+
+    update_errores() {
+        this.cantidad_errores++;
+    }
+
+    agregar_deespawn(time) {
+        this.deespawn_time[this.cantidad_seleccionados] = time;
+    }
+
+    calculoPuntaje() {
+        var salida = []
+        salida[0] = this.calculo_puntaje_final();
+
+        //Defino el flavour text en base al puntaje que consiguio
+        switch (salida[0]) {
+            case "F":
+                salida[1] = "Recomendamos que trabajes en tus reflejos, intente nuevamente."
+                break
+            case "E":
+                salida[1] = "Todavia se puede mejorar, intente nuevamente."
+                break
+            case "D":
+                salida[1] = "Todavia se puede mejorar, intente nuevamente."
+                break
+            case "C":
+                salida[1] = "Todavia se puede mejorar, intente nuevamente."
+                break
+            case "B":
+                salida[1] = "Bien! Tiene reflejos por encima de la media, intente nuevamente."
+                break
+            case "A":
+                salida[1] = "Excelente! Intente nuevamente con una mayor dificultad o mas objetivos."
+                break
+            case "S":
+                salida[1] = "Sus reflejos y punteria son excelentes, felicitaciones!"
+                break
+        }
+
+        return salida;
+    }
+
+
+    promedio_apuntado() {
+        //Genero un arreglo de diferencias
+        var diff = []
+        for (i = 0; i < this.deespawn_time.length - 1; i++) {
+            var elem_i = parseFloat(this.deespawn_time[i])
+            var elem_i1 = parseFloat(this.deespawn_time[i + 1])
+            diff.push(parseFloat((elem_i1 - elem_i).toFixed(2)))
+        }
+        var suma = 0
+        for (i = 0; i < diff.length; i++) {
+            suma += diff[i]
+        }
+        return (suma / diff.length).toFixed(2)
+    }
+
+
+    calculo_puntaje_final() {
+        var valores_facil = [2, 2.422, 2.535, 2.637, 2.836, 2.889]
+        var valores_medio = [2.339, 2.533, 2.709, 2.817, 2.959, 3.101]
+        var valores_dificil = [2.298, 2.353, 2.512, 2.670, 2.762, 3.219]
+            //Hago el calculo del puntaje a mapear
+        var parte_t = ((1 / 3) * (this.tiempo_transcurrido / this.cantidad_targets_max))
+        var parte_e = ((1 / 3) * (this.cantidad_errores / this.cantidad_targets_max))
+        var parte_p = ((1 / 3) * (this.promedio_diferencias))
+        this.puntaje_final = 1 / (parte_p + parte_e + parte_t)
+
+        //Hago el chequeo de dificultad y asigno el puntaje final
+        switch (this.dificultad) {
+            case "1":
+                return this.seleccion_puntaje(valores_facil)
+                break;
+
+            case "2":
+                return this.seleccion_puntaje(valores_medio)
+                break;
+
+            case "3":
+                return this.seleccion_puntaje(valores_dificil)
+                break;
+        }
+
+    }
+
+
+    seleccion_puntaje(array_punt) {
+
+        if (this.puntaje_final <= array_punt[0]) {
+            return "F"
+        } else if (this.puntaje_final > array_punt[0] && this.puntaje_final <= array_punt[1]) {
+            return "E"
+        } else if (this.puntaje_final > array_punt[1] && this.puntaje_final <= array_punt[2]) {
+            return "D"
+        } else if (this.puntaje_final > array_punt[2] && this.puntaje_final <= array_punt[3]) {
+            return "C"
+        } else if (this.puntaje_final > array_punt[3] && this.puntaje_final <= array_punt[4]) {
+            return "B"
+        } else if (this.puntaje_final > array_punt[4] && this.puntaje_final <= array_punt[5]) {
+            return "A"
+        } else {
+            return "S"
+        }
+    }
+
+}
